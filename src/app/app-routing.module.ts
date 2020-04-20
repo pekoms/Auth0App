78e55fab17e9fcc773333f78dd95e59098a5708c
Pekoms <alejandropi301196@gmail.com>
2020-04-20 19:27:40 +0200

    Loggin works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'precios', component: PreciosComponent},
   {path: 'protegida', component: ProtegidaComponent},
+  {path: 'callback', component: ProtegidaComponent},
   {path: '**', pathMatch:'full',redirectTo: 'home'}
 
 
